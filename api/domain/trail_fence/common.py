9f4f6a5f0768bf731dc1b9ac9d07fe0dcbcb7375
dsoares <daniel_soares2007@hotmail.com>
2023-04-13 15:39:05 -0300

    adjustments2

--- api/domain/trail_fence/common.py
+++ api/domain/trail_fence/common.py
@@ -1,10 +1,10 @@
 def fix_key(key: str):
-      for i in range(len(key)):
-    if str(i+1) in key:
-      continue
-    e = i+2
+    for i in range(len(key)):
+        if str(i+1) in key:
+            continue
+        e = i+2
     while str(i+1) not in key:
-      if str(e) in key:
-        key = key.replace(str(e),str(i+1))
-      e=e+1
-  return key
+        if str(e) in key:
+            key = key.replace(str(e),str(i+1))
+        e=e+1
+    return key
